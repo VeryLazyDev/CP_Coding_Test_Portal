ff53afeccd27560f990fa8c2671042fe2516b8e3
orion <nnmm5122004@gmail.com>
2025-10-17 15:53:22 +0630

    :fire: delete backend folder

--- backend/prisma/migrations/20251017061656_change_option_json_to_json/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `options` on table `Questions` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Questions" ALTER COLUMN "options" SET NOT NULL;
--- backend/user/api/APIRegister.mjs
+++
@@ -1,6 +0,0 @@
-import Express from "express";
-import authApi from "./auth.mjs";
-
-const RegisterRoutes = Express.Router();
-RegisterRoutes.use("/auth", authApi);
-export default RegisterRoutes;
