306cd91c08e19543fe8baf045c4ec557fe917929
Phyo <phyoheinko.phk1@gmail.com>
2025-10-20 12:04:25 +0630

    :bug: Fixed ERROR - fasle is not defined in register user

--- frontend/app/src/constants/Lazyload.jsx
+++ frontend/app/src/constants/Lazyload.jsx
@@ -1,9 +1,21 @@
 import { lazy } from "react";
-import PageLoader from "../components/core/Pageloader";
+import PageLoader from "../components/core/PageLoader";
 
-export const UserLayout=PageLoader(lazy(()=>import("@/features/user/layout/UserLayout.jsx")));
-export const HomePage=PageLoader(lazy(()=>import("@/features/user/pages/Homepage.jsx")));
-export const AuthLayout=PageLoader(lazy(()=>import("@/features/auth/layout/AuthLayout.jsx")));
-export const LoginPage=PageLoader(lazy(()=>import("@/features/auth/pages/LoginPage.jsx")))
-export const AdminLayout=PageLoader(lazy(()=>import("@/features/admin/layout/AdminLayout.jsx")));
-export const DashboardPage=PageLoader(lazy(()=>import("@/features/admin/pages/DashboardPage.jsx")))
+export const UserLayout = PageLoader(
+    lazy(() => import("@/features/user/layout/UserLayout.jsx")),
+);
+export const HomePage = PageLoader(
+    lazy(() => import("@/features/user/pages/HomePage.jsx")),
+);
+export const AuthLayout = PageLoader(
+    lazy(() => import("@/features/auth/layout/AuthLayout.jsx")),
+);
+export const LoginPage = PageLoader(
+    lazy(() => import("@/features/auth/pages/LoginPage.jsx")),
+);
+export const AdminLayout = PageLoader(
+    lazy(() => import("@/features/admin/layout/AdminLayout.jsx")),
+);
+export const DashboardPage = PageLoader(
+    lazy(() => import("@/features/admin/pages/DashboardPage.jsx")),
+);
