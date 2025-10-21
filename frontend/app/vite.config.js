0f8171811539ef424f13a2a60ece4bf299451b0d
Lwin Min Thein <lwinthein679@gmail.com>
2025-10-21 14:27:23 +0630

    fix vite config

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -4,10 +4,23 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    proxy: {
+      // 👇 This means: any request starting with /api
+      // will be sent to your backend
+      "/api": {
+        target: "http://cp-coding-test-portal.onrender.com", // change this to your backend URL/port
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    // 👇 Fixes 404 errors on page refresh when using React Router
+    historyApiFallback: true,
+  },
 });
