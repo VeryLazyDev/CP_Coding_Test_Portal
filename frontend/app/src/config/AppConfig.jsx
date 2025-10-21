b13bb1018db28bd24c9c96b1a8070ef7b4c90c0f
Phyo <phyoheinko.phk1@gmail.com>
2025-10-21 13:40:00 +0630

    :bug: Fix

--- frontend/app/src/config/AppConfig.jsx
+++ frontend/app/src/config/AppConfig.jsx
@@ -1,8 +1,11 @@
-
-export const AppConfig={
-        BASE_URL:"http://localhost:5173",
-        API_URL :"http://localhost:8080",
-        APP_NAME:"City Properties Coding Placement Test"
-}
-
-
+// export const AppConfig={
+//         BASE_URL:"http://localhost:5173",
+//         API_URL :"http://localhost:8080",
+//         APP_NAME:"City Properties Coding Placement Test"
+// }
+//Prod
+export const AppConfig = {
+    BASE_URL: "",
+    API_URL: "",
+    APP_NAME: "City Properties Coding Placement Test",
+};
